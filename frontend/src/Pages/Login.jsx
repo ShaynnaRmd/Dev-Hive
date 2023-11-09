ebf24b410c87a2caad2651dc62b54b6254ca356c
Lucas RIMBAULT <lucas.rimbault@tentelemed.com>
2023-11-09 17:38:45 +0100

    Test fetch

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -3,12 +3,24 @@ import { jwtDecode } from "jwt-decode";
 import { Form } from "../components/Form";
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import blopTop from "../assets/svg/blobtop.svg";
+import blopMid from "../assets/svg/blobtop.svg";
+import blopDown from "../assets/svg/blobtop.svg";
+import styles from "../assets/css/Register.module.css";
 
 export function Login() {
   const [inputValues, setInputValues] = useState([]);
   const { type } = useParams();
   const navigate = useNavigate();
 
+  const data = {
+    username: "Lucas",
+    password: "lucas",
+  };
+
+  fetch("http://localhost:3000/users/createaccount", data)
+    .then((response) => response.json())
+    .then((res) => console.log(res));
   const formFields = [
     { type: "email", label: "Email", value: inputValues[0] },
     { type: "password", label: "Mot de passe", value: inputValues[1] },
@@ -19,11 +31,21 @@ export function Login() {
   };
 
   return (
-    <>
+    <div className={styles.container}>
+      <div className={styles.bloptop}>
+        <img src={blopTop} alt="" />
+      </div>
+      <div className={styles.blopmid}>
+        <img src={blopMid} alt="blob-mid" />
+      </div>
+      {/* <div>
+        <img src={blopDown} alt="blob-down" />
+      </div> */}
+
       {type === "student" && <h4>Connexion en tant qu'étudiant</h4>}
       {type === "compagny" && <h4>Connexion en tant qu'entreprise</h4>}
 
-      <GoogleLogin
+      {/* <GoogleLogin
         onSuccess={(credentialResponse) => {
           let response = jwtDecode(credentialResponse.credential);
           //Ajouter response comme token à la bdd ainsi qu'au cookies, à chaque page vrifier si le cookie = celui de bdd, au logout supprimé cookie
@@ -32,7 +54,7 @@ export function Login() {
         onError={() => {
           console.log("Login Failed");
         }}
-      />
+      /> */}
       <Form
         fields={formFields}
         onSubmit={handleFormSubmit}
@@ -56,6 +78,6 @@ export function Login() {
           Vous êtes un étudiant ?
         </p>
       )}
-    </>
+    </div>
   );
 }
