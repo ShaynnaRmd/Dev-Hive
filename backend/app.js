5e2ed40620f474cc462411815d7c3f4f4ecfcb5b
Mimenee <imene.toudeft@hotmail.com>
2023-11-10 16:23:40 +0100

    add hash password and fixed minor errors

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require("mongoose")
-const cors = require('cors');
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const {DB_URI} = require("./config")
+const { DB_URI} = require('./config')
 
 var app = express();
 
 mongoose.connect(DB_URI)
-  .then((date) => console.log({date : "bd connected"}))
+  .then((date) => console.log({date: 'db connected'}))
   .catch((err) => console.log(err))
 
 app.use(logger('dev'));
@@ -20,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors('*'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const userStudent = require("../models/user_student");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -23,8 +24,8 @@ router.post("/login", async function (req, res, next) {
     });
   }
 
-  const check_password = await compareAsync(password, userStudent.password);
-  if (check_password) {
+  const check_password = await compareAsync(password, check_user.password);
+  if (check_password == true) {
     return res.json({ success: true, data: check_user });
   }
 
@@ -34,11 +35,34 @@ router.post("/login", async function (req, res, next) {
 router.post("/createaccount", async function (req, res, next) {
   const { email, password } = req.body;
 
-  const user = await userStudent.create({ email, password });
+  if (email == undefined || password == undefined) {
+    return res.json({ err: true, msg: `rentrez tous les champs` });
+  }
+
+  const check_email = await userStudent.findOne({ email });
+  
+  if (check_email) {
+    return res.json({success: false, msg: `${email} est déjà associé à un compte`})
+  }
+
+  const hash_password = await hashPassword(password, 10)
+
+  const user = await userStudent.create({ ...req.body, password: hash_password });
   return res.json({ success: true, data: user });
 });
 
 module.exports = router;
+
+// Fonction pour hasher le password
+const hashPassword = async(password, saltRounds) => {
+  try{
+    const salt = await bcrypt.genSalt(saltRounds)
+    return await bcrypt.hash(password, salt)
+  } catch (error){
+    cpnsole.log(error)
+  }
+  return null
+}
 
 // Foonction de comparaison mmdp rentré dans le form et mdp dans la bdd
 function compareAsync(password, hash_password) {
