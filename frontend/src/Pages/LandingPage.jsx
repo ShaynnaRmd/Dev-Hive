7b4132ea2f166b639b891f7bdcad15b5fe656fbd
victan78 <116087124+Victan78@users.noreply.github.com>
2023-11-28 10:58:34 +0100

    mise a jour front

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -17,35 +17,38 @@ export function LandingPage() {
     <div className="containerlanding1">
       <img src={blob1} className="blob1" alt="" srcset="" />
       <img src={blob2} className="blob2" alt="" srcset="" />
+    
         <div className="containerOfLandingPage">
-          <h1 className="titrelanding">Découvrez la Révolution de la Mise en Relation Étudiant-Entreprise !</h1>
+          <h1 className="titrelanding">Découvrez la <span>Révolution</span>  de la Mise en Relation <span>Étudiant-Entreprise !</span> </h1>
           <div className="paragraphe">Bienvenue sur notre plateforme innovante, le premier site de mise en relation exclusivement dédié aux étudiants et aux entreprises. Nous sommes fiers de présenter un concept novateur qui bouleverse les règles du jeu : chez nous, ce sont les entreprises qui viennent à la rencontre des talents étudiant.</div>
         </div>
-        <div className="landingBtnContenair"><button id="student">Etudiant</button><button id="compagny">Entreprise</button></div>
-    </div>
-    <div className="containerlanding2">
-   
-     <div className="slach"></div>
-     <div className="whiteslachinvisible"></div>
-     <div className="blackslachinvisible"></div>
-     <img src={blob1} className="blob1" alt="" srcset="" />
-      <img src={blob2} className="blob2" alt="" srcset="" />
-
-      <div className="cardcontenaire">
+        <div className="cardcontenaire">
        
-          <div className="card_info"></div>
-        
-    
+       <div className="card_info">
+        <h2> Une Approche Inversée </h2>
+        <p> Contrairement aux autres plateformes, ici, ce sont les entreprises qui initient le contact. Elles peuvent parcourir les profils d'étudiants, découvrir leurs compétences et leurs aspirations, puis les inviter à rejoindre leurs équipes. C'est un moyen efficace et direct de trouver les talents qui correspondent parfaitement à leurs besoins.</p>
+       </div>
+     
+ 
+   
+       <div className="card_info">
+        <h2>Des Opportunités Personnalisées</h2>
+        <p> Chaque entreprise peut affiner sa recherche en fonction de critères spécifiques tels que les compétences techniques, les domaines d'études, l'expérience professionnelle, etc. Cela garantit des correspondances précises et des opportunités personnalisées pour les étudiants.</p>
+       </div>
       
-          <div className="card_info"></div>
-         
 
-          <div className="card_info"></div>
-          
+       <div className="card_info">
+        <h2> Un Accès Facile et Rapide</h2>
+        <p>
+        Notre interface conviviale permet aux entreprises de naviguer en toute simplicité et de trouver les profils d'étudiants qui répondent à leurs exigences. En quelques clics, elles peuvent envoyer des invitations et entrer en contact avec les futurs leaders de demain.
+        </p>
+       </div>
+       
 
-      </div>
-    
+   </div>
+        <div className="landingBtnContenair"><button id="student">Etudiant</button><h2 style={{color:"black"}}>Vous etes</h2><button id="compagny">Entreprise</button></div>
     </div>
+
     </>
   );
 }
--- frontend/src/Router/App.jsx
+++ frontend/src/Router/App.jsx
@@ -6,9 +6,13 @@ import { Register } from "../pages/Register";
 import { Login } from "../Pages/Login";
 import { Profile } from "../Pages/Profile";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import QuizComponent from "../components/quizz";
+import '../assets/css/quizz.css'
 
 function App() {
+ 
   return (
+    
     <>
       <GoogleOAuthProvider clientId="343194206733-mortn374l872cl807nbln24khv30hu7m.apps.googleusercontent.com">
         <BrowserRouter>
@@ -17,6 +21,7 @@ function App() {
             <Route path="/register/:type" element={<Register />}></Route>
             <Route path="/login/:type" element={<Login />}></Route>
             <Route path="/profil" element={<Profile />}></Route>
+            <Route path="/quiz" element={<QuizComponent language='css'  />}></Route>
             <Route></Route>
           </Routes>
         </BrowserRouter>
